0755d77eedb8df96bd59ebc73d26a08c92412412
CamiMarcos <111790102+CamiMarcos@users.noreply.github.com>
2023-02-17 19:49:50 -0300

    Update game.js
    problema pantalla calavera

--- game.js
+++ game.js
@@ -38,9 +38,9 @@ window.addEventListener('resize', setCanvasSize);
 
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
-      canvasSize = window.innerWidth * 0.8;
+      canvasSize = window.innerWidth * 0.9;
     } else {
-      canvasSize = window.innerHeight * 0.8;
+      canvasSize = window.innerHeight * 0.9;
     };
     
       canvasSize = Number(canvasSize.toFixed(0));
