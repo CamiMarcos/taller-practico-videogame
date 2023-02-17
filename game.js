34fc66aeed6e3110db7174b4149c312bdfdaa062
CamiMarcos <111790102+CamiMarcos@users.noreply.github.com>
2023-02-17 19:59:01 -0300

    Update game.js
    fixed problems!

--- game.js
+++ game.js
@@ -38,9 +38,9 @@ window.addEventListener('resize', setCanvasSize);
 
 function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
-      canvasSize = window.innerWidth * 0.9;
+      canvasSize = window.innerWidth * 0.7;
     } else {
-      canvasSize = window.innerHeight * 0.9;
+      canvasSize = window.innerHeight * 0.7;
     };
     
       canvasSize = Number(canvasSize.toFixed(0));
